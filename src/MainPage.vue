d6653e735e1cbac8eca767caa452f110b6b49bb4
李谢贻 <572383082@qq.com>
2023-11-19 17:25:17 +0800

    fix: 新增footer

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { Button, Space, Avatar } from 'ant-design-vue';
 import config from './config.js';
-const { tel } = config;
-const defaultMessage = '我秦始皇！挪车！';
+const data = new URLSearchParams(window.location.search);
+const tel = data.get('tel') || config.tel;
+const message = data.get('message');
+const defaultMessage = message || '我秦始皇！挪车！';
 const moveCar = () => {
     window.location.href = `tel:${tel}`;
 };
@@ -12,7 +14,7 @@ const postMessage = () => {
 </script>
 
 <template>
-    <div class="main-page flex items-center justify-center w-full h-full">
+    <div class="main-page flex flex-col items-center justify-center w-full h-full">
         <Space direction="vertical" align="center" :size="40">
             <Space>
                 <Avatar class="avatar" :size="100" src="/avatar.jpeg"> </Avatar>
@@ -25,5 +27,21 @@ const postMessage = () => {
                 <div class="h-[100px]"></div>
             </Space>
         </Space>
+        <div
+            class="flex mt-8 flex-col justify-center items-center gap-1 !text-default-600 dark:!text-default-500 font-extralight text-sm"
+        >
+            <p>
+                备案号:&nbsp;
+                <a
+                    class="text-sm"
+                    target="_blank"
+                    href="https://beian.miit.gov.cn/"
+                    rel="noreferrer"
+                >
+                    鄂ICP备2021009860号
+                </a>
+            </p>
+            <p>blackCell&nbsp;2021-present</p>
+        </div>
     </div>
 </template>
